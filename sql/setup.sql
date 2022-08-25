bc174c0d7fda41acaf3c0c87fcc7f1ac265eadc4
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-24 18:14:52 -0700

    added todos table to setup sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,6 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS todos CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
@@ -8,4 +9,12 @@ CREATE TABLE users (
   last_name TEXT, 
   email TEXT NOT NULL,
   password_hash TEXT NOT NULL
+);
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT NOT NULL,
+  content TEXT NOT NULL,
+  finished BOOLEAN DEFAULT 'false',
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
